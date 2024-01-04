c154d05f04d7a4d21118de4883f448f5c8821bd2
Achraf <63306573+alaamoum@users.noreply.github.com>
2024-01-04 10:57:40 +0100

    feat: add eslintfix all

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,11 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.motion.leap-nvim"}
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.project.nvim-spectre" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,13 +1,30 @@
 -- customize mason plugins
+
+local utils = require "astronvim.utils"
+
 return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "tsserver", "eslint" })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("eslint_fix_creator", { clear = true }),
+        desc = "Create autocommand in buffers where eslint attaches",
+        callback = function(args)
+          if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "eslint" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              desc = "Fix all eslint errors",
+              buffer = args.buf,
+              group = vim.api.nvim_create_augroup(("eslint_fix_%d"):format(args.buf), { clear = true }),
+              callback = function()
+                if vim.fn.exists ":EslintFixAll" > 0 then vim.cmd.EslintFixAll() end
+              end,
+            })
+          end
+        end,
       })
     end,
   },
@@ -18,8 +35,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettierd",
+        "eslint-lsp",
+        "stylua",
       })
     end,
   },
