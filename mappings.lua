f74c1315f2705bdbb1f67e8aa4f5fa738144486a
Achraf <63306573+alaamoum@users.noreply.github.com>
2024-02-05 09:22:27 +0100

    feat: add split

--- mappings.lua
+++ mappings.lua
@@ -3,10 +3,17 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
 return {
   -- first key is the mode
   n = {
     -- second key is the lefthand side of the map
+    ["<leader>sv"] = {
+      "<cmd>vsplit<cr>",
+    },
+    ["<leader>sh"] = {
+      "<cmd>split<cr>",
+    },
     ["s"] = false,
     ["<S-l>"] = {
       function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
